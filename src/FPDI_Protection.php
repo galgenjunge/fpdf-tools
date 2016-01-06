6b5001e45b3797e6bb84b834476ea844e531635c
galgenjunge <hallo-sascha@gmx.net>
2016-01-06 23:43:18 +0100

    Update FPDI_Protection.php

--- src/FPDI_Protection.php
+++ src/FPDI_Protection.php
@@ -15,6 +15,7 @@
 	 *                                                                           *
 	 ****************************************************************************/
 	namespace Galgenjunge\FPDI;
+	use setasign/fpdi;
 
 	class FPDI_Protection extends FPDI {
 
